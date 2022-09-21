cb4c7bd2716f75801bdcb5ba0d7e218f9e38c346
Jonas Busto <jonasrex00@gmail.com>
2022-09-20 23:20:31 -0300

    registro funcionando

--- script/registro.js
+++ script/registro.js
@@ -3,47 +3,59 @@ function validarRegistro(){
     let usuario = document.getElementById ('usuario').value;
     //let emailUsuario = document.getElementById ('emailUsuario').value;
     let contraseña = document.getElementById ('contraseña').value;
-    let validarContraseña = document.getElementById ('validarContraseña').value;
-    let localSTG = JSON.parse (localStorage.getItem('usuario')) || []
+    let validarContraseña = document.getElementById ('validarContraseña').value
+    //traer valor al localstg. parse transforma a objeto
+    let localSTG = JSON.parse(localStorage.getItem('Usuarios')) || []
+    
     const array = []
 
-    if (usuario === ''|| contraseña === ''|| validarContraseña ===''){
+    if (usuario.trim() === ''|| contraseña.trim() === ''|| validarContraseña.trim() ===''){        
         Swal.fire({
-        title: 'Formulario en blanco',
+        title: 'CAMPOS INCOMPLETO',
         icon: 'warning',
-        confirmButtonText: 'Aceptar',
+        confirmButtonText: 'ACEPTAR'
      })
-    }
-    else {
+    } else {
         /*buscar usuario existente*/
-        for (let i= 0; i <localSTG.lenght; i++){
+        for (let i= 0; i <localSTG.length; i++){
             const usuarioEnLocalSTG = localSTG[i];
             array.push(usuarioEnLocalSTG)
         }
         //ver si hay mismos usuarios
-      let usuarioExistente = localSTG.filter(local => local.usuario === usuario)
+      let usuarioExistente = localSTG.filter(local => local.usuario.trim() === usuario.trim())
        
       //preguntar si agrego sweetalert 'usuario ya existe' o alert, console log
 
         //crear usuario
-
-        if (usuarioExistente.lenght === 0){
+console.log(usuarioExistente)
+        if (usuarioExistente.length === 0){
             if (contraseña === validarContraseña){
-            array.push( {id: localSTG.length + 1, usuario, contraseña, role: 'admin'})
-            localStorage.setItem('usuario', JSON.stringify(array))
+            array.push({id: localSTG.length + 1, usuario: usuario.trim(), contraseña: contraseña.trim(), role: 'usuario', habilitado: false})
+            localStorage.setItem('Usuarios', JSON.stringify(array))
             console.log (array)
+            Swal.fire({
+                title: 'REGISTRO EXITOSO',
+                icon: 'success',
+                html: 'Debe esperar a que el <b>Administrador</b> lo de <b>ALTA</b> en el sistema para poder <b>AUTENTICARSE</b> al mismo.',
+                confirmButtonText: 'ACEPTAR',
+                 })
+            document.getElementById ('usuario').value=''
+            document.getElementById ('contraseña').value=''
+            document.getElementById ('validarContraseña').value=''
         } else {
             Swal.fire({
-                title: 'contraseñas no son iguales',
-                icon: 'warning',
-                confirmButtonText: 'Aceptar',
+            title: 'LAS CONTRASEÑAS NO SON IGUALES',
+            icon: 'warning',
+            confirmButtonText: 'ACEPTAR',
+            confrimButtonColor: '#3085d6'
              })
         }
-        } else{Swal.fire({
-            title: 'usuario ya existe',
+        } else{
+            Swal.fire({
+            title: 'EL USUARIO YA EXISTE',
             icon: 'warning',
-            confirmButtonText: 'Aceptar',
-         })
+            confirmButtonText: 'ACEPTAR',
+             })
 
         }
         
