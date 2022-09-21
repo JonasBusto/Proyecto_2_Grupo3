97d941bc891e8c696941c4824b6c14dea340ded8
JONAS <jonasrex00@gmail.com>
2022-09-21 18:06:54 -0300

    login con visualizador de contraseña

--- script/login.js
+++ script/login.js
@@ -10,44 +10,72 @@
 //    if (nombre.value === '') {
 //        const errorNombre = document.getElementById('errorNombre');
 //        errorNombre.innerText = 'Ingrese nombre';
- //       } else {
-  //           errorNombre.innerText = '';
- //       }
+//       } else {
+//           errorNombre.innerText = '';
+//       }
 
-  //  if (pass.value === '') {
-    //    const errorPass = document.getElementById('errorPass');
-      //  errorPass.innerText = 'Ingrese contraseña';
-        //} else {
-        //     errorPass.innerText = '';
-        //}
-        
-        //if (pass1.value === '') {
-        //    const errorPass1 = document.getElementById('errorPass1');
-         //   errorPass1.innerText = 'Ingrese contraseña';
-         //   } else {
-         //       errorPass1.innerText = '';
-        //}
-    //}
+//  if (pass.value === '') {
+//    const errorPass = document.getElementById('errorPass');
+//  errorPass.innerText = 'Ingrese contraseña';
+//} else {
+//     errorPass.innerText = '';
+//}
 
-  function iniciarSesion(){
-     let usuario = document.getElementById('usuario').value
-     let contraseña = document.getElementById ('contraseña').value
-     let localSTG = JSON.parse(localStorage.getItem ('usuario')) || []
-     let usuarioExiste = localSTG.filter(local => local.usuario === usuario)
-    
-    //corroboro que el usuario ingresado exista
-     if(usuarioExiste.length > 0){
-        if (usuarioExiste[0].contraseña === contraseña) {
-             localStorage.setItem('id', JSON.stringify(usuarioExiste[0].id))
-             //ternario para redirigiar si son admin o usuario
-             usuarioExiste[0].rol === 'admin'
-              ?
-             location.href = './usuariosABM.html'
-              :
-             //location.href = agregar html vista de usuario
-         }
-     }
- }    
-             //preguntar si se agregar alert con sweet alert o con inner cuando  los campos no estan lleno
-   
-     
+//if (pass1.value === '') {
+//    const errorPass1 = document.getElementById('errorPass1');
+//   errorPass1.innerText = 'Ingrese contraseña';
+//   } else {
+//       errorPass1.innerText = '';
+//}
+//}
+
+function login() {
+  let usuario = document.getElementById('usuario').value
+  let contraseña = document.getElementById('contraseña').value
+  let localSTG = JSON.parse(localStorage.getItem('Usuarios')) || []
+  let usuarioExistente = localSTG.filter(local => local.usuario.toLowerCase().trim() === usuario.toLowerCase().trim())
+
+  if (usuario.trim() === '' || contraseña.trim() === '') {
+    Swal.fire({
+      title: 'CAMPOS INCOMPLETO',
+      icon: 'warning',
+      confirmButtonText: 'ACEPTAR'
+    })
+  } else {
+    Swal.fire({
+      title: 'El usuario o contraseñas son invalidas',
+      icon: 'warning',
+      confirmButtonText: 'ACEPTAR'
+    })
+  }
+  //corroboro que el usuario ingresado exista
+  if (usuarioExistente.length > 0) {
+    if (usuarioExistente[0].contraseña === contraseña) {
+      usuarioExistente[0].role === 'admin'
+        ?
+        //localStorage.setItem('id', JSON.stringify(usuarioExiste[0].id))
+        //ternario para redirigiar si son admin o usuario
+        location.href = '/usuariosABM.html'
+        :
+        location.href = '/paginaUsuario.html'
+    }
+
+  }
+
+}
+
+
+// Permitir funcionalidad para visualizar o no la contraseña haciendo click en el icono del "ojo"
+const btnVisualizar = document.getElementById("div-visualizar-contraseña");
+const iconoEye = document.getElementById("icono-eye");
+const contraseña = document.getElementById('contraseña');
+
+btnVisualizar.addEventListener("click", ()=>{
+  if(contraseña.type === "password") {
+    contraseña.type = "text";
+  } else {
+    contraseña.type = "password";
+  }
+  iconoEye.classList.toggle("fa-eye");
+  iconoEye.classList.toggle("fa-eye-slash");
+})
