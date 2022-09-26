510c6ba85e9f5929dc4639fe9c92905b2f116187
Jonas Busto <jonasrex00@gmail.com>
2022-09-26 18:02:24 -0300

    abmUsuarios agregado a script

--- script/abmUsuarios.js
+++ script/abmUsuarios.js
@@ -1,17 +1,19 @@
 let listausuario = JSON.parse(localStorage.getItem("Usuarios"));
+listausuario.reverse()
 let selecfila = document.getElementById("filas");
 selecfila.innerHTML = listausuario.map((usuario) =>
-    `<tr>
+    `
+    <tr>
         <th scope="row">${usuario.id}</th>
         <td>${usuario.usuario}</td>
         <td>
             <div>
                 <div class="w-100 d-flex justify-content-center">
-                    <a class="mx-auto anchor-btn-habilitar desactivar" data-bs-toggle="modal"
-                    data-bs-target="#modalHabilitarUsuario1">HABILITAR</a>
+                    <a class="mx-auto anchor-btn-habilitar desactivar ${usuario.habilitado === false ? " " : this.class = "d-none"}" data-bs-toggle="modal"
+                    data-bs-target="#modalHabilitarUsuario${usuario.id}">HABILITAR</a>
                 </div>
 
-                <div class="modal fade" data-bs-backdrop="static" id="modalHabilitarUsuario1"
+                <div class="modal fade" data-bs-backdrop="static" id="modalHabilitarUsuario${usuario.id}"
                     tabindex="-1" aria-labelledby="modalHabilitarUsuario1Label" aria-hidden="true">
                     <div class="modal-dialog">
                         <div class="modal-content">
@@ -40,7 +42,7 @@ selecfila.innerHTML = listausuario.map((usuario) =>
 
         <!-- MODAL ELIMINAR USUARIO -->
         <div>
-            <a>
+            <a class=${usuario.role === "admin" ? "deshabilitado" : " "} >
                 <i data-bs-toggle="modal" data-bs-target="#modalEliminarUsuario${usuario.id}">
                     <img src="/img/user_delete_icon.png" alt="icono_basurero" width="30px" height="30px"/>
                 </i>
@@ -65,7 +67,7 @@ selecfila.innerHTML = listausuario.map((usuario) =>
                                 <button type="button" class="btn mx-4" data-bs-dismiss="modal">
                                     Cancelar
                                 </button>
-                                <button type="button" class="mx-4 btn" id="btnEliminarUsuario1">
+                                <button type="button" class="mx-4 btn" id="btnEliminarUsuario1" onclick="eliminarUsuario(${usuario.id})">
                                     Eliminar
                                 </button>
                             </div>
@@ -100,18 +102,18 @@ selecfila.innerHTML = listausuario.map((usuario) =>
                             </p>
                             <div class="d-flex flex-column">
                                 <div class="mb-3">
-                                    <label class="form-label fw-bold">Nombre de Usuario Actual:  <b>${usuario.usuario}</b></label>
-                                    <input type="text" class="form-control" id="modif_nombre_usuario">
+                                    <label class="form-label fw-bold">Nombre de Usuario</label>
+                                    <input type="text" class="form-control" id="modif_usuario${usuario.id}" value="${usuario.usuario}">
                                 </div>
                                 <div class="mb-3">
-                                    <label class="form-label fw-bold">Contraseña Actual: <b>${usuario.contraseña}</b></label>
-                                    <input type="text" class="form-control" id="modif_contraseña_usuario">
+                                    <label class="form-label fw-bold">Contraseña</label>
+                                    <input type="text" class="form-control" id="modif_contraseña${usuario.id}" value="${usuario.contraseña}">
                                 </div>
                                 <div class="d-flex flex-column mb-3 ">
-                                    <label class="form-label fw-bold">Rol Actual: <b>${usuario.role}</b></label>
-                                    <select id="${usuario.role}" class="form-select" aria-label="Default select example">
+                                    <label class="form-label fw-bold">Rol Actual: <b>${usuario.role}</label>
+                                    <select id="modif_role${usuario.id}" class="form-select" aria-label="Default select example">
                                         <option value="usuario">usuario</option>
-                                        <option value="administrador">administrador</option>
+                                        <option value="admin">administrador</option>
                                      </select>
                                 </div>
                             </div>
@@ -124,7 +126,7 @@ selecfila.innerHTML = listausuario.map((usuario) =>
                                 <button class="btn mx-4" data-bs-dismiss="modal">
                                     Cancelar
                                 </button>
-                                <button class="mx-4 btn" id="">
+                                <button class="mx-4 btn" onclick="modificarUsuario(${usuario.id})">
                                     Guardar Cambios
                                 </button>
                                  <p class="text-success d-none" id="">
@@ -141,11 +143,40 @@ selecfila.innerHTML = listausuario.map((usuario) =>
    `
   ).join("");
 
-// Inhabilitar el administrador para eliminación y modificacion de nombre de usuario
+// Lista Reverse
+  listausuario.reverse()
 
+// Función Eliminar Usuario
+const eliminarUsuario = (id) => {
+    let array = []
 
+    for (let i = 0; i < listausuario.length; i++) {
+        const usuario = listausuario[i];
+        if (usuario.id !== id) {
+            array.push(usuario)
+        }
+    }
+    localStorage.setItem('Usuarios', JSON.stringify(array))
+    location.href = './usuariosABM.html'
+}
 
 // Función Modificar Usuario
-// const modificarusuario = document.getElementById(`modalModificarusuario${usuario.id}`);
+const modificarUsuario = (id) => {
+    let usuario1 = document.getElementById(`modif_usuario${id}`).value
+    let constraseña1 = document.getElementById(`modif_contraseña${id}`).value
+    let role1 = document.getElementById(`modif_role${id}`).value
+    let array = []
 
+    for (let i = 0; i < listausuario.length; i++) {
+        const usuario = listausuario[i];
+        if (usuario.id === id) {
+            array.push({ id, usuario: usuario1, contraseña: constraseña1, role: role1, habilitado: true })
+        } else {
+            array.push(usuario)
+        }
+    }
 
+    localStorage.setItem('Usuarios', JSON.stringify(array))
+    location.href = './usuariosABM.html'
+
+}
